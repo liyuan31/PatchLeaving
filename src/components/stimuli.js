8bc815860a01251bc9439268bd6e1cee849991de
Walden31 <admin@waldenli.com>
2022-02-12 23:38:08 -0500

    Create function

--- src/components/stimuli.js
+++ src/components/stimuli.js
@@ -4,6 +4,49 @@
  * @author Walden Y. Li
  * @version 1.0 (2/11/2022)
  */
-export function create_a_T() {
-    console.log("T");
+
+import Display from "./display";
+import { Line } from "./shape";
+
+/**
+ *
+ * @param {number} x : x coordinate of the center of the shape
+ * @param {number} y : y coordinate of the center of the shape
+ * @param {number} orientation : 0, 1, 2, 3 for right, up, left, down
+ */
+export function create_a_T(x, y, orientation) {
+    const result = new Display();
+    // Draw the tail
+    result.add_a_line(
+        new Line(
+            x -
+                (this._setting.T_tail_length + this._setting.TL_stroke_width) /
+                    2,
+            y,
+            x +
+                (this._setting.T_tail_length + this._setting.TL_stroke_width) /
+                    2,
+            y,
+            this._setting.TL_stroke_color,
+            this._setting.TL_stroke_width,
+            undefined,
+            undefined,
+            `rotate(${orientation * -90}, ${x}, ${y})`
+        ) // counter-clockwise rotation
+    );
+    // Draw the head
+    result.add_a_line(
+        new Line(
+            x - this._setting.T_tail_length / 2,
+            y - this._setting.T_head_width / 2,
+            x - this._setting.T_tail_length / 2,
+            y + this._setting.T_head_width / 2,
+            this._setting.TL_stroke_color,
+            this._setting.TL_stroke_width,
+            undefined,
+            undefined,
+            `rotate(${orientation * -90}, ${x}, ${y})`
+        )
+    );
+    return result;
 }
