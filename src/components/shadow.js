90dd45b2b6ac3609d4f6780b0d142907a4eb6226
Walden31 <admin@waldenli.com>
2022-02-28 12:20:33 -0500

    Add shadow function

--- src/components/shadow.js
+++ src/components/shadow.js
@@ -1,7 +1,7 @@
 /**
  * Functions that handles shadow randering.
  *
- * @credit https://css-tricks.com/drawing-realistic-clouds-with-svg-and-css/
+ * Adapted from https://css-tricks.com/drawing-realistic-clouds-with-svg-and-css/
  */
 export function initialize() {
     function rn(from, to) {
@@ -17,8 +17,9 @@ export function initialize() {
         for (let i = 0; i < max; ++i) {
             ret.push(`
         ${rn(1, 100)}vw ${rn(1, 100)}vh ${rn(20, 40)}vmin ${rn(1, 20)}vmin
-        ${rs("#808080", "#696969", "#A9A9A9", "#C0C0C0", "#D3D3D3")}
+        ${rs("#0A0A0A", "#1E1E1E", "#333333", "#474747", "#5E5E5E")}
       `);
+            //        ${rs("#808080", "#696969", "#A9A9A9", "#C0C0C0", "#D3D3D3")}
         }
         return ret.join(",");
     }
@@ -29,4 +30,8 @@ export function initialize() {
     }
 
     window.addEventListener("load", update);
+}
+
+export function update() {
+    document.querySelector("#cloud").style.boxShadow = boxShadows(100);
 }
